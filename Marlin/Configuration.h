8c49c6914bbcd21e0a690c6c8ce2f103ff6a586f
WheresWaldo <bud.hammerton@gmail.com>
2017-06-15 14:06:08 -0400

    Turn off Fan PWM
    FAN_SOFT_PWN should be disabled so that UV LED control is not affected with low level pulsing.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -722,7 +722,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; //--BH set to true to invert the log
 // Use software PWM to drive the fan, as for the heaters. This uses a very low frequency
 // which is not ass annoying as with the hardware PWM. On the other hand, if this frequency
 // is too low, you should also increment SOFT_PWM_SCALE.
-#define FAN_SOFT_PWM  //--BH
+//#define FAN_SOFT_PWM
 
 // Incrementing this by 1 will double the software PWM frequency,
 // affecting heaters, and the fan if FAN_SOFT_PWM is enabled.
