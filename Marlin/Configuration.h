96080a738b56a7cc627c390efaa8656054a9cfcd
bringho <bringho@gmail.com>
2017-07-16 15:43:45 +0200

    Don't move buildplate below home!
    Stop the buildplate from slamming into the FEP, messing up the alignment of the endstop when I erroneously  move it to low manually.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -291,7 +291,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define Y_HOME_DIR -1
 #define Z_HOME_DIR -1
 
-#define min_software_endstops false //--BH If true, axis won't move to coordinates less than HOME_POS.
+#define min_software_endstops true //--BR If true, axis won't move to coordinates less than HOME_POS.
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 //============================= Bed Auto Leveling ===========================
